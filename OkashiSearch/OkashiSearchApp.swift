8cead9bd52f66f77e09eb80abd6572954d15346b
i-qbu <iqbu.lab@gmail.com>
2024-01-31 20:10:43 +0900

    Initial Commit

---
+++ OkashiSearch/OkashiSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  OkashiSearchApp.swift
+//  OkashiSearch
+//
+//  Created by 渡辺大智 on 2024/01/31.
+//
+
+import SwiftUI
+
+@main
+struct OkashiSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
